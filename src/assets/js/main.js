290a26937825bcc269d2179797bb86b16bfe799c
Aude Schaff <contact@audeschaff.be>
2021-05-22 19:10:04 +0200

    FEAT: HTML landing done, HTML structure play.html, sass architecture + variables + type done

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,58 +1,37 @@
 "use strict";
 
-// DARK MODE + LOCAL STORAGE
-var buttonMobile = document.querySelector(".menu__darkbtn");
-var lien = document.querySelectorAll(".link--menu");
-buttonMobile.addEventListener("click", dark);
-var attribute = localStorage.getItem("data-theme");
+var ctx = document.querySelector('#myChart').getContext('2d');
 
-if (attribute == "dark") {
-    document.documentElement.setAttribute("data-theme", "dark");
-} else {
-    document.documentElement.setAttribute("data-theme", "light");
-}
-
-function dark() {
-    var update = document.documentElement.getAttribute("data-theme");
-
-    if (update == "dark") {
-        document.documentElement.setAttribute("data-theme", "light");
-        localStorage.setItem("data-theme", "light");
-        buttonMobile.innerHTML = "Dark mode";
-    } else {
-        document.documentElement.setAttribute("data-theme", "dark");
-        localStorage.setItem("data-theme", "dark");
-        buttonMobile.innerHTML = "Light mode";
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Mustang1', 'Mustang2'],
+        datasets: [{
+            data: [12, 19],
+            label: {
+                display: false
+            },
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'],
+            maxBarThickness: 20,
+        }],
+    },
+    options: {
+        plugins: {
+            legend: false,
+        },
+        scales: {
+            x: {
+                display: false,
+                grid: {
+                    offset: false
+                }
+            },
+            y: {
+                display: false,
+            }
+        },
+        indexAxis: 'y',
     }
-}
-
-
-// Burger menu 
-
-let currentPage = document.querySelector(".menu__burger");
-
-if (currentPage != null) {
-    let navButton = document.querySelector(".menu__burger");
-    navButton.addEventListener("click", toggleNavigation);
-
-    function toggleNavigation() {
-        if (document.body.hasAttribute("data-menu")) {
-            document.body.removeAttribute("data-menu");
-        } else {
-            document.body.setAttribute("data-menu", true);
-        }
-    }
-
-    for (let i = 0; i < lien.length; i++) {
-
-        lien[i].addEventListener("click", closeMenu);
-        function closeMenu() {
-            document.body.removeAttribute("data-menu");
-        };
-    }
-
-}
-
-// DATE 
-var year = new Date().getFullYear();
-document.querySelector(".copyright").innerHTML = "Aude Schaff © " + year;
+});
